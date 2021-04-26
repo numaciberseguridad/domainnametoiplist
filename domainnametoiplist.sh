daf49d8e2464528cd605329b9b3070894dc12495
numaciberseguridad <73398798+numaciberseguridad@users.noreply.github.com>
2021-04-26 23:57:04 +0200

    Update domainnametoiplist.sh

--- domainnametoiplist.sh
+++ domainnametoiplist.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-# Converter.sh by @xdavidhu
-# This is a script inspired by the Bug Hunter's Methodology 3 by @Jhaddix
+# domainnametoiplist.sh by numaciberseguridad
 # With this script, you can convert domain lists to resolved IP lists without duplicates.
-# Usage: ./converter.sh [domain-list-file] [output-file]
+# Usage: ./domainnametoiplist.sh [domain-list-file] [output-file]
 
-echo -e "[+] Converter.sh by @xdavidhu\n"
+echo -e "[+] Converter.sh by numaciberseguridad\n"
 if [ -z "$1" ] || [ -z "$2" ]; then
-  echo "[!] Usage: ./converter.sh [domain-list-file] [output-file]"
+  echo "[!] Usage: ./domainnametoiplist.sh [domain-list-file] [output-file]"
   exit 1
 fi
 echo "[+] Resolving domains to IPs..."
